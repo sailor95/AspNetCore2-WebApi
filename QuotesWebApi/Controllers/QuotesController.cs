9a1fc7cf910730ad0b7e79826ad45b60818a581a
David Huang <david.huang@getac.com.tw>
2019-08-22 13:53:49 +0800

    Init

---
+++ QuotesWebApi/Controllers/QuotesController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using QuotesWebApi.Models;
+
+namespace QuotesWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuotesController : ControllerBase
+    {
+        List<Quote> _quotes = new List<Quote>()
+        {
+            new Quote(){Id=0,Author="David Huang", Description="The brain is wider than sky.", Title="Inspiration"},
+            new Quote(){Id=1,Author="Richard Batch",Description="The story of Rich boy.",Title="Funny Stories"}
+        };
+
+        [HttpGet]
+        public IEnumerable<Quote> Get()
+        {
+            return _quotes;
+        }
+
+        [HttpPost]
+        public void Post([FromBody]Quote quote)
+        {
+            _quotes.Add(quote);
+        }
+
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]Quote quote)
+        {
+            _quotes[id] = quote;
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            _quotes.RemoveAt(id);
+        }
+    }
+}
