8f3b18bb2b54c2e861ec59174c7a6780db1e1524
David Huang <david.huang@getac.com.tw>
2019-08-23 10:50:17 +0800

    Add xml formatter dependency and service

--- QuotesWebApi/Startup.cs
+++ QuotesWebApi/Startup.cs
@@ -29,6 +29,7 @@ namespace QuotesWebApi
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
             services.AddDbContext<QuotesDbContext>(option => option.UseSqlServer(@"Data Source =(localdb)\MSSQLLocalDB;Initial Catalog=QuotesDb;"));
+            services.AddMvc().AddXmlSerializerFormatters();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
